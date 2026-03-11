c2ef4994e6c371833a11e145579f68c4292f8ac1
unknown <manassaini.9451@gmail.com>
2026-03-11 10:03:28 +0530

    make navbar responsive

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,34 +1,124 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import React, { useState } from "react";
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Box
+} from "@mui/material";
+
+import MenuIcon from "@mui/icons-material/Menu";
+
 import { Link } from "react-router-dom";
 
 function Navbar() {
 
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+  const menuItems = [
+    { text: "Dashboard", path: "/" },
+    { text: "Employees", path: "/employees" },
+    { text: "Attendance", path: "/attendance" }
+  ];
+
   return (
-    <AppBar position="static">
 
-      <Toolbar>
+    <>
+      <AppBar position="static">
 
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          HRMS Lite
-        </Typography>
+        <Toolbar>
 
-        <Button color="inherit" component={Link} to="/">
-          Dashboard
-        </Button>
+          <Typography
+            variant="h6"
+            sx={{ flexGrow: 1 }}
+          >
+            HRMS Lite
+          </Typography>
 
-        <Button color="inherit" component={Link} to="/employees">
-          Employees
-        </Button>
+          {/* Desktop Menu */}
 
-        <Button color="inherit" component={Link} to="/attendance">
-          Attendance
-        </Button>
+          <Box sx={{ display: { xs: "none", md: "block" } }}>
 
-      </Toolbar>
+            {menuItems.map((item) => (
 
-    </AppBar>
+              <Button
+                key={item.text}
+                color="inherit"
+                component={Link}
+                to={item.path}
+              >
+                {item.text}
+              </Button>
+
+            ))}
+
+          </Box>
+
+          {/* Mobile Menu Button */}
+
+          <IconButton
+            color="inherit"
+            edge="end"
+            onClick={toggleDrawer}
+            sx={{ display: { xs: "block", md: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+        </Toolbar>
+
+      </AppBar>
+
+      {/* Mobile Drawer */}
+
+      <Drawer
+        anchor="right"
+        open={open}
+        onClose={toggleDrawer}
+      >
+
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={toggleDrawer}
+        >
+
+          <List>
+
+            {menuItems.map((item) => (
+
+              <ListItem
+                button
+                key={item.text}
+                component={Link}
+                to={item.path}
+              >
+
+                <ListItemText primary={item.text} />
+
+              </ListItem>
+
+            ))}
+
+          </List>
+
+        </Box>
+
+      </Drawer>
+
+    </>
   );
+
 }
 
 export default Navbar;
